2b74a6a0355aa3a9b5a9bbd2c2be006c5a8c4c02
Danil00524 <jerry.lewis.green@gmail.com>
2024-06-22 22:18:20 +0400

    up

--- src/_inputs/settings.ts
+++ src/_inputs/settings.ts
@@ -2,12 +2,15 @@ import { Settings } from '../types';
 
 export const SETTINGS: Settings = {
   // Столбец по которому будет производиться поиск
-  // 'ID' |  'Wallet Address' | 'Priv Key' | 'Serial Number' | 'Mnemonic' | 'OKX Wallet'
-  // 'Binance Wallet' | 'Pontem Wallet' | 'Pontem Priv Key' | 'Scam' | 'Ads Pass'
-  fieldToSearch: 'ID',
+  // ID, evm_address, evm_priv_key, evm_mnemonic, okx_evm, binance_evm, bitget_evm, bybit_evm, aptos_address
+  // aptos_priv_key, okx_aptos, solana_address, solana_priv_key, is_scam, ads_serial_number, ads_pass,proxy
+  fieldToSearch: 'evm_address',
 
   // Столбцы, которые будут выведены в found.csv, чтоб вывести все, необходимо оставить []
-  fieldsToReceive: [],
+  // fieldsToReceive: ['ID', 'evm_address', 'evm_priv_key', 'bitget_evm', 'okx_evm'],
+  fieldsToReceive: ['ID', 'solana_wallet', 'solana_privkey'],
+  // fieldsToReceive: ['ID', 'ads_serial_number', 'ads_pass', 'evm_priv_key'],
+  // fieldsToReceive: ['ID', 'ads_serial_number', 'ads_pass', 'evm_mnemonic'],
 
   filters: {
     // Использовать ли вместо search.csv фильтра
@@ -19,8 +22,9 @@ export const SETTINGS: Settings = {
     // Scam: true - найдет те поля, где значение true
     // Linea: null - найдет те поля, где значение пустое
     // Transactions: '>5' - найдет те поля, где значение 5 и более
-    // walletAddress: '!null' - найдет не пустые поля,
-    // walletAddress: 'in=0x' - найдет те поля, которые имеют указанное значение
+    // ads_serial_number: '!null', // - найдет не пустые поля,
+    blabla: '!null',
+    // zkSync: 'in=0x', // - найдет те поля, которые имеют указанное значение
   },
 
   // По какому столбцу выполнять сортировку
